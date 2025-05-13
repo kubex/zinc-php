ff3cead14beb10addbd4099feba0d988c1f30cf0
Alan Jones <alan.jones@chargehive.com>
2025-05-13 12:28:52 +0100

    Create `Item` component

---
+++ src/Components/Item/Item.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Kubex\Zinc\Components\Item;
+
+use Kubex\Zinc\Attributes\Size;
+use Kubex\Zinc\ZincHtmlElement;
+use Packaged\Ui\Html\HtmlElement;
+
+class Item extends ZincHtmlElement
+{
+  protected $_tag = 'zn-item';
+
+  public function caption(string $content): static
+  {
+    $this->setAttribute('caption', $content);
+    return $this;
+  }
+
+  public function stacked(): static
+  {
+    $this->setAttribute('stacked', true);
+    return $this;
+  }
+
+  public function size(Size $size): static
+  {
+    $this->setAttribute('size', $size->value);
+    return $this;
+  }
+
+  public function editOnHover(): static
+  {
+    $this->setAttribute('editOnHover', true);
+    return $this;
+  }
+
+  public function icon(string $content): static
+  {
+    $this->setAttribute('icon', $content);
+    return $this;
+  }
+
+  public function value(string $content): static
+  {
+    $this->setAttribute('value', $content);
+    return $this;
+  }
+
+  public function inline(): static
+  {
+    $this->setAttribute('inline', true);
+    return $this;
+  }
+
+  public function noPadding(): static
+  {
+    $this->setAttribute('noPadding', true);
+    return $this;
+  }
+}
