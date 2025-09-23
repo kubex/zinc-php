804b1e59a81672681514aafc6973eddce4560a50
Jleagle <jimeagle@gmail.com>
2025-09-23 15:41:49 +0100

    Add table meta

---
+++ src/Builder/DataTable/Meta.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kubex\Zinc\Builder\DataTable;
+
+use JsonSerializable;
+
+class Meta implements JsonSerializable
+{
+  public function __construct(
+    protected int $_perPage,
+    protected int $_total,
+    protected int $_page,
+  )
+  {
+  }
+
+  public function jsonSerialize(): array
+  {
+    return [
+      'per_page'    => $this->_perPage,
+      'total'       => $this->_total,
+      'page'        => $this->_page,
+      'total_pages' => $this->_total > 0 && $this->_perPage > 0 ? (int)ceil($this->_total / $this->_perPage) : 1,
+    ];
+  }
+}
